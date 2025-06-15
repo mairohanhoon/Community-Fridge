e2ec6b586ca0343f69ca57e269593a2d5e513093
mairohanhoon <rohantiwari2709@gmail.com>
2025-06-16 00:12:14 +0530

    Update Navbar logo image source to use new asset with removed background

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const MyNavbar = () => {
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="" className="h-8" alt="Logo" />
+                <img src="https://raw.githubusercontent.com/mairohanhoon/Community-Fridge/refs/heads/main/frontend/src/assets/remove.photos-removed-background.png" className="h-8" alt="Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Community Fridge</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
