02cebc5c047012c96006a0768256167b9e3c8564
mairohanhoon <rohantiwari2709@gmail.com>
2025-06-15 23:15:19 +0530

    initialize frontend with React, Vite, and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+function App() {
+
+  return (
+
+<div>App
+  <Routes>
+    <Route path="/" element />
+  </Routes>
+</div>
+  );
+}
+
+export default App;
