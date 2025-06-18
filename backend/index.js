c4ca03dfa41942392ba29c052491ef5d0c202d2e
mairohanhoon <rohantiwari2709@gmail.com>
2025-06-19 02:15:32 +0530

    fix: Simplify CORS configuration and correct environment variable name in frontend

--- backend/index.js
+++ backend/index.js
@@ -23,13 +23,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: ["https://community-fridge-z7xx.vercel.app", "https://community-fridge-z7xx.vercel.app/login", "https://localhost:3000"],
     credentials: true,
   })
 );
